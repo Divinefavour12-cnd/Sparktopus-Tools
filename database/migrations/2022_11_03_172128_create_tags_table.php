bc1ce64a38d960011cb185c0dec761a1f1b7a17f
Divinefavour12-cnd <chijiokedivine07@gmail.com>
2026-03-04 14:06:40 +0100

    Reorder drop statements in migration down method

--- database/migrations/2022_11_03_172128_create_tags_table.php
+++ database/migrations/2022_11_03_172128_create_tags_table.php
@@ -1,16 +1,9 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
@@ -18,37 +11,27 @@ return new class extends Migration
             $table->boolean('status')->default(true);
             $table->timestamps();
         });
-
         Schema::create('tag_translations', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('locale')->index();
             $table->unsignedBigInteger('tag_id');
-            $table->string('name', 100)->index('name');
-            $table->string('slug', 100)->index('slug');
+            $table->string('name', 100)->index();
+            $table->string('slug', 100)->index();
             $table->string('title', 250)->nullable();
             $table->mediumText('description')->nullable();
-
             $table->unique(['tag_id', 'locale']);
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
         });
-
         Schema::create('taggables', function (Blueprint $table) {
             $table->unsignedBigInteger('tag_id')->index();
             $table->morphs('taggable');
-
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
         });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        Schema::dropIfExists('tags');
-        Schema::dropIfExists('tag_translations');
         Schema::dropIfExists('taggables');
+        Schema::dropIfExists('tag_translations');
+        Schema::dropIfExists('tags');
     }
 };
