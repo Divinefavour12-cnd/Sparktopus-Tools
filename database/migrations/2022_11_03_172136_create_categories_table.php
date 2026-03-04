c775404fc8c26a7ff7533d4856e16e14b5bea013
Divinefavour12-cnd <chijiokedivine07@gmail.com>
2026-03-04 13:54:48 +0100

    Refactor category migration for clarity and order

--- database/migrations/2022_11_03_172136_create_categories_table.php
+++ database/migrations/2022_11_03_172136_create_categories_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
     /**
@@ -15,36 +13,32 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->integer('parent')->index('parent')->nullable();
+            $table->integer('parent')->index()->nullable();
             $table->string('type')->index();
-            $table->integer('order')->index('order')->default(0);
+            $table->integer('order')->index()->default(0);
             $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
-
         Schema::create('category_translations', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('locale')->index();
             $table->unsignedBigInteger('category_id');
-            $table->string('name', 100)->index('name');
-            $table->string('slug', 100)->index('slug');
+            $table->string('name', 100)->index();
+            $table->string('slug', 100)->index();
             $table->string('title', 250)->nullable();
             $table->longText('description')->nullable();
             $table->string('meta_title', 250)->nullable();
             $table->mediumText('meta_description')->nullable();
             $table->text('icon')->nullable();
-
             $table->index(['category_id', 'locale']);
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
-
         Schema::create('catables', function (Blueprint $table) {
             $table->unsignedBigInteger('category_id')->index();
             $table->morphs('catable');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
-
     /**
      * Reverse the migrations.
      *
@@ -52,8 +46,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
-        Schema::dropIfExists('category_translations');
         Schema::dropIfExists('catables');
+        Schema::dropIfExists('category_translations');
+        Schema::dropIfExists('categories');
     }
 };
